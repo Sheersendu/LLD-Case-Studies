50009e96435cd2d6eb7cd59e9e166821381678af
Sheersendu <shibrata15@gmail.com>
2023-11-01 14:48:31 +0530

    All uncommitted files

---
+++ TicTacToe/strategies/RandomPlayingStrategy.java
@@ -0,0 +1,11 @@
+package TicTacToe.strategies;
+
+import TicTacToe.models.Board;
+import TicTacToe.models.BoardCell;
+
+public class RandomPlayingStrategy implements PlayingStrategy{
+	@Override
+	public BoardCell play(Board board) {
+		return null;
+	}
+}
